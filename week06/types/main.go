f4f52e5ebe348d2bca19ad89e9d11651674159d1
bw7178 <bw7178@naver.com>
2025-09-29 11:49:23 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
+	//var name strings
+	//var name = "Kim Inha"
+	//name="Kim Inha"
+	//var name = "kim Inha"
+	var name float64
+	name = 2.71
+	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(name)
 	fmt.Println(math.Ceil(2.71))
 	fmt.Println(strings.Title("head first go"))
 }
